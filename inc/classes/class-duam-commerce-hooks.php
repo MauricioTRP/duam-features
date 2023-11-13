d147c65e8fb0d5406e42a36ee90bc9713a41af80
Mauricio <mauriciofb@gmail.com>
2023-11-12 21:25:08 -0300

    Custom hook to proceed to checkout button

--- inc/classes/class-duam-commerce-hooks.php
+++ inc/classes/class-duam-commerce-hooks.php
@@ -19,6 +19,7 @@ class Duam_Commerce_Hooks {
         /**
          * Add actions
          */
+        add_action( 'wp_footer', [ $this, 'listar_hooks_activos'] );
         add_action('woocommerce_order_status_changed', [ $this, 'change_order_state_if_coupon' ], 10, 2);
     }
 
@@ -38,4 +39,18 @@ class Duam_Commerce_Hooks {
             $order->update_status('completed');
         }
     }
+    
+    public function listar_hooks_activos() {
+        global $wp_filter;
+        echo '<pre>';
+        foreach ($wp_filter as $tag => $hook) {
+            echo '<strong>' . $tag . '</strong>' . ': ';
+            foreach ($hook as $priority => $functions) {
+                echo $priority . ' ';
+            }
+            echo '<br>';
+        }
+        echo '</pre>';
+    }
 }
+
